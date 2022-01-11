66e5af534e69ae52a5391964f73cde9d6cb621fc
Artur Alcoverro <artur.bcn1998@gmail.com>
2022-01-11 16:03:33 +0100

    Constants per les files i columnes dels diferents algoritmes

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,8 +7,8 @@ import edu.salleurl.arcade.words.model.WordsSolver;
 
 public class Main {
 
-    private static final int LABYRINTH_COLUMNS = 84;
-    private static final int LABYRINTH_ROWS = 84;
+    private static final int LABYRINTH_COLUMNS = 99;
+    private static final int LABYRINTH_ROWS = 99;
     private static final int WORDS_COLUMNS = 12;
     private static final int WORDS_ROWS = 12;
 
@@ -27,6 +27,17 @@ public class Main {
                 .setLabyrinthSolver(getClassSolutionLabyrinth(menu.getLabyrinthoOption()));
 
         builder.build().run();
+
+
+/*        builder.setLabyrinthColumns(LABYRINTH_COLUMNS)
+                .setLabyrinthRows(LABYRINTH_ROWS)
+                .setLabyrinthSolver(new BacktrackingLabyrinthSolver())
+                .build().run();
+
+        builder.setLabyrinthColumns(LABYRINTH_COLUMNS)
+                .setLabyrinthRows(LABYRINTH_ROWS)
+                .setLabyrinthSolver(new BacktrackingLabyrinthSolverImproved())
+                .build().run();*/
 
 //        for (int i = 0; i < 10; i++) {
 //            System.out.println("DIM: " + 10 * (i + 1) + "/" + 10 * (i + 1));
