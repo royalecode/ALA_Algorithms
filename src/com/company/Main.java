1ad089487fc83da67b6caab994f1a59045f0be91
Artur Alcoverro <artur.bcn1998@gmail.com>
2022-01-11 11:13:06 +0100

    Constants  del Main + Bucle de laberints

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,30 +7,41 @@ import edu.salleurl.arcade.words.model.WordsSolver;
 
 public class Main {
 
+    private static final int LABYRINTH_COLUMNS = 84;
+    private static final int LABYRINTH_ROWS = 84;
+    private static final int WORDS_COLUMNS = 12;
+    private static final int WORDS_ROWS = 12;
+
     public static void main(String[] args) {
         Menu menu = new Menu();
         menu.start();
 
-        Arcade arcade = new ArcadeBuilder()
-                .setLabyrinthColumns(84)
-                .setLabyrinthRows(84)
-                .setWordsColumns(12)
-                .setWordsRows(12)
-                // Opcional, per fixar un input en comptes d'obtenir-ne un d'aleatori
-                .setSeed(42)
-                // DemoLabyrinthSolver implementa LabyrinthSolver
-                .setLabyrinthSolver(getClassSolutionLabyrinth(menu.getLabyrinthoOption()))
-                // DemoWordsSolver implementa WordsSolver
-                .setWordsSolver(getClassSolutionWords(menu.getWordsOption()))
-                .build();
-        arcade.run();
+        ArcadeBuilder builder = new ArcadeBuilder().setSeed(42);
 
+        builder.setWordsColumns(WORDS_COLUMNS)
+                .setWordsRows(WORDS_ROWS)
+                .setWordsSolver(getClassSolutionWords(menu.getWordsOption()));
+
+        builder.setLabyrinthColumns(LABYRINTH_COLUMNS)
+                .setLabyrinthRows(LABYRINTH_ROWS)
+                .setLabyrinthSolver(getClassSolutionLabyrinth(menu.getLabyrinthoOption()));
+
+        builder.build().run();
+
+//        for (int i = 0; i < 10; i++) {
+//            System.out.println("DIM: " + 10 * (i + 1) + "/" + 10 * (i + 1));
+//            builder.setLabyrinthColumns(10 * (i + 1))
+//                    .setLabyrinthRows(10 * (i + 1))
+//                    .setLabyrinthSolver(getClassSolutionLabyrinth(menu.getLabyrinthoOption()));
+//
+//            builder.build().run();
+//        }
     }
 
     private static LabyrinthSolver getClassSolutionLabyrinth(int option) {
         if (option == 1) return new BacktrackingLabyrinthSolver();
         if (option == 2) return new BacktrackingLabyrinthSolverImproved();
-        //if (option == 3) return new BranchAndBoundLabryinthSolver();
+        if (option == 3) return new BranchAndBoundLabryinthSolver();
         return null;
     }
 
@@ -39,6 +50,4 @@ public class Main {
         if (option == 2) return new DemoWordsSolver();
         return null;
     }
-
-
 }
